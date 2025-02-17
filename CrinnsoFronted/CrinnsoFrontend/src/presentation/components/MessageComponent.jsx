b621000ef1590d02978ea2218ef3ff0f85aa1bfb
LuisHerrera9701 <luis.herrera@easy-rez.com>
2025-02-16 18:35:06 -0600

    quitar codigo

--- CrinnsoFronted/CrinnsoFrontend/src/presentation/components/MessageComponent.jsx
+++ CrinnsoFronted/CrinnsoFrontend/src/presentation/components/MessageComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 import { useMessageComponent } from "./useMessageComponent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
